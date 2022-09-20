412fc1a88155c472b224d37064dcbadf4046202d
FloLcs <florianlcs16@gmail.com>
2022-09-20 18:16:36 +0200

    animation a finir lien et hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
+// Locomotive JS
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
+});
+
+// Swiper JS
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.2,
+  grabCursor: true,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    425: {
+      slidesPerView: 1.8,
+    },
+    600: {
+      slidesPerView: 2.3,
+    },
+    768: {
+      slidesPerView: 2.8,
+    },
+    1024: {
+      slidesPerView: 3.2,
+    },
+  },
 });
